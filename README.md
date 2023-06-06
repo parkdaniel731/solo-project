dc520b61cc7178bf62241cef07016391241485db
Daniel Park <danielpark@Daniels-MacBook-Air.local>
2023-06-06 12:58:40 -0700

    adds dev environment setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Solo Project
+
+## Makin Hwatu Yo
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hwatu Go Stop</h1>
+      {/* <Table /> */}
+    </div>
+  );
+}
+
+export default App;
